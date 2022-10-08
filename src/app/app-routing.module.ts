523c35bb25ffd94a0a0d0ea06c628eff15d37470
TEO <alfredo.ssm@gmail.com>
2022-10-08 19:04:27 -0300

    issue #11 done layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './pages/menu/menu.component';
+import { StatementsComponent } from './pages/statements/statements.component';
 
 const routes: Routes = [
   { path: 'menu',
     pathMatch: 'full',
     component: MenuComponent
   },
+  { path: 'interview-trainer', pathMatch: 'full', component: StatementsComponent },
   { path: '**', pathMatch: 'full', redirectTo: '/menu' },
 ];
 
